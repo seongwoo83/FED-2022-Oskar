c32d09ff72c807b323bc2ff1bf3e288c67a2a229
seongwoo83 <jeongseongwoo83@daum.net>
2023-02-13 10:54:57 +0900

    02/13 10:54

---
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -0,0 +1,19 @@
+//CGV PJ 메인페이지 JS - main.js
+
+/* 요소 선택 함수 */
+const qs = x => document.querySelector(x);
+const qsa = x => document.querySelectorAll(x);
+
+
+
+
+
+/* ############ 로드구역 ############### */
+window.addEventListener("DOMContentLoaded",()=>{
+    console.log("로딩완료");
+
+
+
+
+
+})/*################## 로드구역 ##################*/

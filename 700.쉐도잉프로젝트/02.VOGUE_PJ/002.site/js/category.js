7c82e1e1778cb3bee1c365051c7142a1681b965a
seongwoo83 <jeongseongwoo83@daum.net>
2023-03-02 12:20:16 +0900

    03/02 12:20

---
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/category.js
@@ -0,0 +1,9 @@
+/* 보그 PJ 카테고리 페이지 JS - category.js */
+
+window.addEventListener("DOMContentLoaded", loadFn);
+
+function loadFn(){
+    console.log("로딩완료");
+    
+
+}
---
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/linksys.js
@@ -0,0 +1,9 @@
+/* 보그 PJ 링크시스템 JS - linksys.js */
+
+window.addEventListener("DOMContentLoaded", linkFn);
+
+function linkFn(){
+    console.log("로딩완료");
+    
+
+}

164c0ae185894ce94c0c8e48ddeac2e6cad2a442
seongwoo83 <jeongseongwoo83@daum.net>
2023-03-14 10:48:36 +0900

    03/14 10:48

--- 001.HTML학습/10.Ossomae_PJ/js/main.js
+++ 001.HTML학습/10.Ossomae_PJ/js/main.js
@@ -1,10 +1,18 @@
+"use strict";
 // 옷소매 갤러리 JS - main.js
-
 ///////////////// 로딩구역 ///////////////////////
-window.addEventListener("DOMContentLoaded",()=>{
-
+window.addEventListener("DOMContentLoaded", () => {
+    const gbx = document.querySelector(".gbx");
     console.log("로딩완료!");
-
-    
+    document.querySelector(".rb").onclick = () => {
+        console.log("오른쪽");
+        let tg = gbx.querySelectorAll("div");
+        gbx.appendChild(tg[0]);
+    };
+    document.querySelector(".lb").onclick = () => {
+        console.log("왼쪽");
+        let tg = gbx.querySelectorAll("div");
+        gbx.insertBefore(tg[tg.length - 1], tg[0]);
+    };
 }); ///////////// 로딩구역 //////////////////////
 /////////////////////////////////////////////////
---
+++ 001.HTML학습/10.Ossomae_PJ/js/main.ts
@@ -0,0 +1,20 @@
+// 옷소매 갤러리 JS - main.js
+///////////////// 로딩구역 ///////////////////////
+window.addEventListener("DOMContentLoaded",()=>{
+
+    const gbx : any = document.querySelector(".gbx");
+
+    console.log("로딩완료!");
+    document.querySelector(".rb").onclick = ()=>{
+        console.log("오른쪽");
+        let tg : any[] = gbx.querySelectorAll("div");
+        gbx.appendChild(tg[0]);
+    }
+
+    document.querySelector(".lb").onclick = ()=>{
+        console.log("왼쪽");
+        let tg: any[] = gbx.querySelectorAll("div");
+        gbx.insertBefore(tg[tg.length-1], tg[0]);
+    }
+}); ///////////// 로딩구역 //////////////////////
+/////////////////////////////////////////////////

8bb95079775138fd38777be9b2feba77b01f03a5
seongwoo83 <jeongseongwoo83@daum.net>
2023-03-15 10:17:31 +0900

    03/15 10:17

--- 001.HTML학습/10.Ossomae_PJ/js/main.ts
+++ 001.HTML학습/10.Ossomae_PJ/js/main.ts
@@ -1,20 +1,84 @@
 // 옷소매 갤러리 JS - main.js
 ///////////////// 로딩구역 ///////////////////////
-window.addEventListener("DOMContentLoaded",()=>{
-
-    const gbx : any = document.querySelector(".gbx");
-
+window.addEventListener("DOMContentLoaded", () => {
     console.log("로딩완료!");
-    document.querySelector(".rb").onclick = ()=>{
-        console.log("오른쪽");
-        let tg : any[] = gbx.querySelectorAll("div");
-        gbx.appendChild(tg[0]);
-    }
 
-    document.querySelector(".lb").onclick = ()=>{
-        console.log("왼쪽");
+    const gbx: any = document.querySelector(".gbx");
+
+    /* *****************************************
+        함수명: goSlide
+        기능: 이동방향에 따른 요소 이동하기
+    ******************************************/
+   let prot: number = 0;
+    const goSlide: Function = (dir: number) => {
+
+        /* 0. 중복클릭 방지하기 */
+        /* 여러번 클릭 금지 변수: 0일때는 허용, 1일때는 불허용 */
+        if (prot) return;
+        prot = 1;
+        // console.log("나는 들어왔어");
+        setTimeout(() => {
+            prot = 0;
+        }, 400); /* //////////////// */
+
+        /* 1. 호출확인 */
+        console.log(dir);
+
+        /* 2. 대상선정 */
+        /* 이동대상: .gbx>div */
         let tg: any[] = gbx.querySelectorAll("div");
-        gbx.insertBefore(tg[tg.length-1], tg[0]);
-    }
+
+        /* 3. 분기하기 */
+        if (dir) {
+            /* 3-1 오른쪽 버튼 클릭시 */
+            gbx.appendChild(tg[0]);
+        } else {
+            /* 3-2 왼쪽 버튼 클릭시 */
+            gbx.insertBefore(tg[tg.length - 1], tg[0]);
+        }
+    };
+
+    
+
+    /*  오른쪽 버튼 클릭시 */
+    document.querySelector(".rb").onclick = () => {
+        goSlide(1);
+        clearAuto();
+    };
+
+    /*  왼쪽 버튼 클릭시 */
+    document.querySelector(".lb").onclick = () => {
+        goSlide(0);
+        clearAuto();
+    };
+
+     /* 인터벌 함수 멈추기위한 변수 */
+     let autoI : any;
+     /* 타임아웃 함수 지우기위한 변수 */
+     let autoT : any; 
+     
+     /** 
+      * 함수명: autoSlide
+      * 기능: 인터벌 함수로 슬라이드 함수 호출
+      */
+     function autoSlide(){
+         autoI = setInterval(() => goSlide(1), 3000);
+     }
+ 
+     /* 자동넘김 최초호출 */
+     autoSlide();
+ 
+     /** 
+      * 함수명: clearAuto
+      * 기능: 인터벌함수 지우고 다시세팅
+      */
+     function clearAuto(){
+         /* 1. 인터벌 지우기 */
+         clearInterval(autoI);
+         /* 2. 타임아웃도 지우지 않으면 쌓여서 타임아웃 쓰나미 실행*/
+         clearTimeout(autoT);
+         /* 2. 잠시 후 다시 작동하도록 타임아웃 */
+         setTimeout(autoSlide, 5000);
+     }
 }); ///////////// 로딩구역 //////////////////////
 /////////////////////////////////////////////////

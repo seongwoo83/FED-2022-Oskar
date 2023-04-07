e5e4f1ee2b6e371c6142f3935b04fe42d119ff84
seongwoo83 <jeongseongwoo83@daum.net>
2023-04-07 17:08:37 +0900

    04/07 17:08

--- 810.실험실/03.인트로사이트/js/main.js
+++ 810.실험실/03.인트로사이트/js/main.js
@@ -4,10 +4,16 @@
 import AutoScroll from "../../../dist/810.실험실/03.인트로사이트/js/jquery-autoScroll.js"
 
 // 자동스크롤 인스턴스 생성
-
+const autoSc = new AutoScroll();
 
 
 // 스크롤 속도변경
 
+console.log('기존 속도', autoSc.sc_speed);
+autoSc.sc_speed = 500;
+console.log('변경 속도', autoSc.sc_speed);
 // 이징변경
+console.log("기존 이징", autoSc.easing);
+autoSc.easing = "easeOutExpo";
+console.log("변경 이징", autoSc.easing);
 

821d191ffc216d35e64443050f7dac791fe0fcee
seongwoo83 <jeongseongwoo83@daum.net>
2023-04-21 16:04:56 +0900

    04/21 16:04

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/main.js
@@ -69,21 +69,67 @@ const slide = $(".slide");
 
 // 2. 드래그 설정
 slide.draggable({
-    axis: "x"
+    axis: "x" // x축을 기준으로 움직임
 })
 
-// 3. 기준위치 체크후 이동 애니메이션
+// 3. 드래그가 끝난 후 -> dragstop 이벤트 발생 후
+// 기준위치 체크후 이동 애니메이션
 
 // 윈도우 크기 리턴함수
 const reWin = () => $(window).width();
-
-
-// 윈도우 가로크기 
-let winW = reWin();
-console.log("winW", winW);
 
 // 리사이즈 업데이트
 $(window).on("resize", function(){
     winW = reWin();
     console.log("winW", winW);
 })
+
+// 윈도우 가로크기 - left 기준위치 px변환
+let winW = reWin();
+console.log("winW * 0.9: ", winW * 0.9);
+console.log("winW: ", winW);
+console.log("winW * 1.1: ", winW * 1.1);
+
+// 광드래그 방지위해 커버세팅 (show(), hide())
+const cover = $(".cover");
+
+
+// 드래그 끝난 후 이벤트 함수 만들기
+slide.on("dragstop",function(){
+    // 광드래그 방지 커버 
+    cover.show()
+    // slide left위치값
+    let sleft = $(this).offset().left;
+    console.log(sleft);
+
+    // 1. 왼쪽으로 이동 : -110% 미만일때
+    if(sleft < -winW*1.1){
+        slide.animate({
+            left: -winW*2+"px"
+        }, 600, "easeOutQuint", ()=>{
+            // 이동후 맨앞 li 맨뒤로 이동
+            slide.append(slide.find("li").first()).css({left:"-100%"});
+            cover.hide();
+        })
+    }
+    // 2. 오른쪽으로 이동 : -90% 초과일때
+    else if(sleft > -winW*0.9){
+        slide.animate({
+            left: "0px"
+        }, 600, "easeOutQuint",()=>{
+            // 이동후 맨앞 li 맨뒤로 이동
+            slide.prepend(slide.find("li").last()).css({left:"-100%"});
+            cover.hide();
+        })
+    }
+    // 3. 제자리로 이동 : -110% ~ -90%
+    else{
+        slide.animate({
+            left: -winW+"px"
+        }, 600, "easeOutQuint",()=>{
+            cover.hide();
+        })
+    }
+
+
+});

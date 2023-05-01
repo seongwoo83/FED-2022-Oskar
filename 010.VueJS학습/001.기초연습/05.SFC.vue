2e149b654c9d4fadae2a286ffd603a98cca6d33e
seongwoo83 <jeongseongwoo83@daum.net>
2023-05-01 15:49:50 +0900

    05/01 15:49

---
+++ 010.VueJS학습/001.기초연습/05.SFC.vue
@@ -0,0 +1,23 @@
+<!-- html 템플릿 코드 -->
+<template>
+    <div class="mytemp">
+        <span class="tit">{{txt}}</span>
+    </div>
+</template>
+
+<!-- JS 코드 -->
+<script>
+export default(await import("vue").undefineComponent({
+    name: "myTemp",
+    data(){
+        return{
+            txt: "연습"
+        }
+    }
+}))
+</script>
+
+<!--스타일 시트 코드 -->
+<style scoped>
+    span.tit{color:red}
+</style>

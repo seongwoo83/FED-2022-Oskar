707f818bde53a3f7480877c324d59ccc6547203a
seongwoo83 <jeongseongwoo83@daum.net>
2023-05-12 13:18:56 +0900

    05/12

--- 010.VueJS학습/001.기초연습/js/vueData-store.js
+++ 010.VueJS학습/001.기초연습/js/vueData-store.js
@@ -1,3 +1,23 @@
+//  제이슨 데이터 처리를 위해 제이슨 불러오기
+
+/************************************
+    import jsnData from "./goods.json";
+    일반 JS호출과 같은 방식으로 제이슨 파일을 호출하면
+    아래와 같은 MIME 전송형식 type 에러가 발생한다.
+    _________________________________________________________
+    ** Failed to load module script: Expected a JavaScript module script 
+        but the server responded with a MIME type of "application/json". 
+        Strict MIME type checking is enforced for module scripts per HTML spec.
+    ---------------------------------------------------------------------------------
+
+    ((해결방법))
+    import 변수 from 제이슨파일경로 assert{type:"json"}
+
+    assert{type:"json"} -> import시 형식을 지정해주는 객체
+    이것으로 제이슨 형식을 맞춰서 전송타입 오류를 없앰
+************************************/
+import jsnData from "./goods.json" assert{type:"json"};
+
 const store = new Vuex.Store({
     // (1) 데이터 세팅구역
     state:{
@@ -5,12 +25,25 @@ const store = new Vuex.Store({
     }, 
     // (2) 데이터 변경 메서드 구역 : 호출시 commit()사용
     mutations:{
-
+        // 제이슨 데이터 속성 변수 업데이트하기
+        setData(st, pm){ //st- state 변수, pm- 전달변수
+            // state구역의 item변수에 제이슨 데이터 담기
+            st.items = pm;
+            console.log("뮤테", pm);
+        }
     },
     // (3) 백엔드 관련 코딩 비동기처리 메서드 구역 : 호출시 dispatch()사용
     actions:{
-        myAct(헝, 벙){
-            console.log("나의 액션", 헝, 벙);
+        // 제이슨 데이터 로드하기 메서드
+        initData({commit}){ // actions 메서드 전달값으로  {commit}을 사용하면 mutations 구역으로 바로 commit 사용가능
+            // 제이슨 데이터 변수에 할당
+            const result = jsnData;
+            console.log("액션",result);
+
+            // state의 items변수 변경 메서드 호출
+            // mutations의 메서드 호출은 -> commit('메서드명', 전달변수)
+            commit('setData', result);
+
         }
     }
 })
--- 010.VueJS학습/001.기초연습/js/vueData.js
+++ 010.VueJS학습/001.기초연습/js/vueData.js
@@ -1,11 +1,19 @@
 import store from "./vueData-store.js";
 
 // 컴포넌트 생성하기
+// 이미지 src속성 바인드시
+// (v.idx > 50 ? 1 : v.idx) 문장해석
+// 데이터 idx 값이 50초과이면 1을 넣고 아니면 idx데이터 넣기
+
+// axios로 가져온 데이터는 {data:{실제 데이터}} 형식으로 한 번 더 랩핑하여 들어옴
+// 따라서 이 데이터만 사용하려면 $store.state.items.data처럼  data속성까지 써주어야 사용가능함
+// 반면, 뷰엑스 스토어의 actions에서 받은 제이슨 데이터는 원본 그대로 할당하여 들어오므로
+// $store.state.items -> 원본 변수를 바로 사용해야 함
 Vue.component("my-comp",{
     template:`
     <div class="grid">
-        <div v-for="(v,i) in $store.state.items.data">
-            <img v-bind:src="'./img_gallery/'+ v.idx + '.jpg'" alt="dress">
+        <div v-for="(v,i) in $store.state.items">
+            <img v-bind:src="'./img_gallery/'+ (v.idx>50? 1: v.idx) + '.jpg'" alt="dress">
             <aside>
                 <h2>{{v.gname}}</h2>
                 <h3>{{v.gprice}}</h3>
@@ -31,6 +39,10 @@ new Vue({
     created:function(){
         // axios 사용하여 제이슨 데이터 가져오기
         // axios.get(제이슨파일).then(데이터=>담을변수=데이터)
-        axios.get("./js/data.json").then( x => store.state.items = x)
+        // axios.get("./js/goods.json").then( x => store.state.items = x);
+
+        // 뷰엑스 스토어 actions 구역 메서드 initData 호출하기
+        store.dispatch('initData');
+
     }
 })

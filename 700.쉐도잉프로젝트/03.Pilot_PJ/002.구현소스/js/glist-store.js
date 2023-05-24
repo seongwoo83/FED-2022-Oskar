21570d845e7a07378332a1fcc2c35bfd9b569259
seongwoo83 <jeongseongwoo83@daum.net>
2023-05-24 17:19:52 +0900

    05/24

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist-store.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist-store.js
@@ -49,7 +49,97 @@ const store = new Vuex.Store({
             if(dt.mnum+10 > gdata.length){
                 dt.mbtn = false
             }
+        },
+
+
+        // 장바구니 데이터 업데이트 메서드
+        setData(dt, pm){
+            if(localStorage.getItem('cart')==null){
+                localStorage.setItem("cart", "[]")
+            }
+        
+            // 로컬스토리지 객체데이터 가져오기
+            // 입력된 데이터는 문자형 객체이므로
+            // 다시 파싱하여 원래 객체로 복원한다!
+            let org = localStorage.getItem("cart");
+            org = JSON.parse(org);
+            // console.log("변환객체:", org);
+            
+            // 배열뒤에 밀어넣기 메서드 : push(값)
+            org.push(
+                dt.gdata[pm]
+            );
+            // 객체를 문자형으로 변환후 로컬스토리지에 반영
+            localStorage.setItem("cart", JSON.stringify(org));
+            console.log("반영후:", localStorage.getItem("cart"));
+
+            // 카트애니메이션 후 버튼을 등장시켜 카트리스트까지 연동
+            this.commit('cartAni', org.length)
+
+        },
+
+        cartAni(dt, pm){
+            console.log('카트애니', pm);
+
+            $("#mycart").remove();
+
+            $("body").append(`
+                <img id="mycart" src="./images/mycart.gif"
+                title="${pm}개의 상품이 카트에 있습니다">
+            `);
+
+            $("#mycart").css({
+                position:"absolute",
+                top:"50%",
+                left:"50%",
+                transform:"translate(-50%, -50%)",
+                cursor: "pointer",
+                zIndex:"99999",
+            }).delay(3000).animate({
+                top:"5%",
+                left:"80%",
+                width:"50px"
+            },1000, "easeInExpo").on("click", function(){
+                // body에 카트리스트 요소 넣기
+                $("body").append(`
+                    <section id="cartlist"></section>
+                `);
+                // 생성된 카트리스트에 테이블 넣기
+                $("#cartlist").html(`
+                    <a href="#" class="cbtn cbtn2">×</a>
+                    <table>
+                        <caption>
+                            <h1>카트 리스트</h1>
+                        </caption>
+                        <tr>
+                            <th>번호</th>
+                            <th>상품명</th>
+                            <th>상품코드</th>
+                            <th>단가</th>
+                            <th>수량</th>
+                            <th>합계</th>
+                            <th>삭제</th>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                `)
+            })
+
+
+
+
         }
+
+
+
     },
 });
 

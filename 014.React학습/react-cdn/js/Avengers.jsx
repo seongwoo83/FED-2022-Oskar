7cf271ab24ccad663ee4cebbe6f3bfbe8ed8f398
seongwoo83 <jeongseongwoo83@daum.net>
2023-05-24 13:20:44 +0900

    05/24

---
+++ 014.React학습/react-cdn/js/Avengers.jsx
@@ -0,0 +1,15 @@
+// 컴포넌트 파일 분할 구현하기
+function Avengers() {
+    return (
+    <React.Fragment>
+        <h1>어벤저스여 모두 모여라</h1>
+        <img src="./images/ab1.jpg" alt="어벤저스" />
+        <img src="./images/ab2.jpg" alt="어벤저스" />
+        <img src="./images/ab3.jpg" alt="어벤저스" />
+        <img src="./images/ab4.jpg" alt="어벤저스" />
+    </React.Fragment>
+    );
+}
+
+// 컴포넌트 내보내기
+export default Avengers;

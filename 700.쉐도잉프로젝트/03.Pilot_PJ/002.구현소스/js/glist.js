a16d8a9dd3b3002a023252d6c7ab7b1f52d6c96f
seongwoo83 <jeongseongwoo83@daum.net>
2023-05-26 14:42:36 +0900

    05/26

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist.js
@@ -29,12 +29,6 @@ new Vue({
     store,
     router,
     mounted(){
-        // 라우터 강제 실행하기
-        this.$router.push('/glist');
-        // push(실행할 vue router 경로)
-        
-        // 최초 체크박스 메서드 실행해야 리스트 나옴!
-        store.commit('resCheck');
         menuFn();
         $("#logo").click(() => (location.href = "index.html"));
 
@@ -49,5 +43,14 @@ new Vue({
                 // cnt - 카트아이템 개수
                 // opt - 세팅 옵션번호(초기 css값 선택 옵션) - 0( 오른쪽 위 작게 ) / 1(정중앙 크게)
         }
+    },
+    //  뷰 인스턴스 생성직후 세팅구역
+    created(){ // 뷰 데이터 프리세팅 / 라우트 세팅 미리 하는 구역
+        // 라우터 강제 실행하기
+        this.$router.push('/glist');
+        // push(실행할 vue router 경로)
+        
+        // 최초 체크박스 메서드 실행해야 리스트 나옴!
+        store.commit('resCheck');
     }
 })

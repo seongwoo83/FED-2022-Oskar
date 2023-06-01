51b1ebc264c8bb1bd0176ae62e60c3fd5799774d
seongwoo83 <jeongseongwoo83@daum.net>
2023-06-01 12:22:13 +0900

    06/01

--- 005.jQuery학습/003.응용구현/jq_board/js/list.js
+++ 005.jQuery학습/003.응용구현/jq_board/js/list.js
@@ -15,13 +15,12 @@ let bdata = JSON.parse(localStorage.getItem("bdata"));
 // console.log(bdata);
 
 // 3-2 게시판 리스트 생성하기
-// 데이터 순서: 번호, 글제목, 글쓴이, 작성일, ㄹ
-let blist = "";
+// 데이터 순서: 번호, 글제목, 글쓴이, 작성일, 조회수
 
 // 페이징 번호
-let pgnum = 1;
+// let pgnum = 1;
 // 페이지 단위
-let pgblock = 10;
+// let pgblock = 10;
 // 시작번호 생성
 // -> (pgnum-1)*pgblock
 // 끝번호 생성
@@ -31,32 +30,56 @@ let pgblock = 10;
     함수명: bindList
     기능: 페이지별 리스트를 생성하여 바인딩함
 ************************************/
-function bindList(num){
-    pgnum = num
+function bindList(pgnum, pgblock){
     // 1. 반복문으로 코드 작성
+    let blist ="";
     for (let i = (pgnum - 1) * pgblock; i < pgnum * pgblock; i++) {
-        blist += `
-            <tr>
-                <td>${bdata[i]["idx"]}</td>
-                <td>${bdata[i]["tit"]}</td>
-                <td>${bdata[i]["writer"]}</td>
-                <td>${bdata[i]["date"]}</td>
-                <td>${bdata[i]["cnt"]}</td>
-            </tr>
-        `;
+        // 마지막 번호 한계값 조건으로 마지막 페이지 데이터 존재하는 데이터까지만 바인딩하기
+        if(i< bdata.length){
+            blist += `
+                <tr>
+                    <td>${bdata[i]["idx"]}</td>
+                    <td>
+                        <a href="view.html?idx=${bdata[i]["idx"]}">${bdata[i]["tit"]}</a>
+                        </td>
+                    <td>${bdata[i]["writer"]}</td>
+                    <td>${bdata[i]["date"]}</td>
+                    <td>${bdata[i]["cnt"]}</td>
+                </tr>
+            `;
+        }
     }
     // 2 반복문으로 작성한 코드 리스트에 넣기
     $("#board tbody").html(blist);
 
     // 3. 페이징 블록 만들기
-    // 전체 페이지 번호수 계산하기
+    // 3-1 전체 페이지 번호수 계산하기
     // 전체 레코드수 / 페이지 단위수 + (나머지 있으면 한페이지 더)
     let pgtotal= Math.floor(bdata.length / pgblock);
     let pgadd = bdata.length % pgblock;
-    
+    // 페이징 코드 변수
+    let pgcode = "";
+
+    // 3-2 페이징 코드 만들기
+    if(pgadd!=0) pgtotal++;
+
+    for(let i =1; i<=pgtotal; i++){
+        pgcode += pgnum == i?  `<b>${i}</b>` : `<a href="#">${i}</a>`
+
+        if(i != pgtotal) pgcode += ` | `;
+    };
+
+    // 3-3 페이징 코드 넣기
+    $(".paging").html(pgcode)
+
+    // 3-5 이벤트 링크 생성하기
+    $(".paging a").on("click",function(e){
+        e.preventDefault();
+        bindList($(this).text(), 9);
+    })
 }
 // console.log(blist);
 $(() => {
     // 최초 리스트 호출
-    bindList(1);
+    bindList(1,9);
 });

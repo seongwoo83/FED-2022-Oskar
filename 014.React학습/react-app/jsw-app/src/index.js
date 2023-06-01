17eae257607fec84896bb4540f3e76b7ad62a84c
seongwoo83 <jeongseongwoo83@daum.net>
2023-06-01 13:20:21 +0900

    06/01

--- 014.React학습/react-app/jsw-app/src/index.js
+++ 014.React학습/react-app/jsw-app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Test from './test';
+import Ban from './dc/ban';
 
 
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(<Test />);
+root.render(<Ban />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

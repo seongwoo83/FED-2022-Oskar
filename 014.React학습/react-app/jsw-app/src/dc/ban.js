30dc746541f293aaa0d49ce580200877dc0747a7
seongwoo83 <jeongseongwoo83@daum.net>
2023-06-02 10:56:36 +0900

    06/02

--- 014.React학습/react-app/jsw-app/src/dc/ban.js
+++ 014.React학습/react-app/jsw-app/src/dc/ban.js
@@ -1,5 +1,17 @@
 import "./css/ban.css";
 import ban_data from "./data/banner";
+import $ from "jquery";
+
+$(()=>{
+    // 1. 버튼 클릭시 이동기능 구현
+    $(".abtn").on("click",function(){
+        let isB = $(this).is(".rb");
+        // console.log('오른쪽? ', isB);
+        if(isB){
+            
+        }
+    });////////click
+});
 
 function MakeList(props) {
     return (
@@ -21,11 +33,21 @@ function Ban(props) {
     // sel_data에 담긴값은 data/banner.js 객체의 배열값
     return (
         <div className="banner">
+            {/* 이동 슬라이드 */}
             <ul className="slider">
                 {
                     sel_data.map((x, i)=><MakeList rec={x} key={i}/>)
                 }
             </ul>
+            {/* 이동버튼+슬라이드 불릿 : 슬라이드가 두 장 이상일 때 */}
+            {
+                /* 조건식 && 코드: 조건식이 true 일때 코드 출력 */
+                sel_data.length > 1 &&
+                <>
+                    <button className="abtn lb">＜</button>
+                    <button className="abtn rb">＞</button>
+                </>
+            }
         </div>
     );
 } /////////// Ban

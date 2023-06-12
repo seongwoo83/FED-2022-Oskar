701bc4d57c1dcb793a47fd38a6a95b3b461c7581
seongwoo83 <jeongseongwoo83@daum.net>
2023-06-12 09:38:56 +0900

    06/12

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Main.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Main.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // DC 메인 페이지 컴포넌트
 import React from "react";
 import mdata from "./data/menu";
@@ -5,6 +6,19 @@ import Ban from "./modules/ban";
 import MenuBtn from "./modules/MenuBtn";
 import VidIntro from "./modules/VidIntro";
 import Vidswipe from "./modules/VidSwipe";
+import { Tilt } from 'react-tilt';
+
+const defaultOptions = {
+	reverse:        false,  // reverse the tilt direction
+	max:            35,     // max tilt rotation (degrees)
+	perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+	scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
+	speed:          1000,   // Speed of the enter/exit transition
+	transition:     true,   // Set a transition on enter/exit.
+	axis:           null,   // What axis should be disabled. Can be X or Y.
+	reset:          true,    // If the tilt effect has to be reset on exit.
+	easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+}
 
 const Main = () =>{
     return(
@@ -12,7 +26,7 @@ const Main = () =>{
             <Ban cat={"Main"+Math.ceil(Math.random()*2)}/>
             <div className="menubx" >
                 {mdata.map((x, i)=>{
-                    return <MenuBtn rec={x} key={i} /> 
+                    return<MenuBtn rec={x} key={i} />
                 })}
             </div>
             <VidIntro pg={"main"}/>

be8919d67ebc70f5330222b5a31528c2b6774e8f
seongwoo83 <jeongseongwoo83@daum.net>
2023-06-23 10:22:20 +0900

    06/23

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/Search.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/Search.js
@@ -3,6 +3,8 @@ import '../css/search.css'
 import CatList from './CatList';
 import cat_data from '../data/cat';
 import { useState } from 'react';
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function jqfn(){
     $(()=>{
@@ -11,8 +13,17 @@ function jqfn(){
 }
 function Search(){
 
-        //  데이터 선택하기
+    //  데이터 선택하기
     let [sdt, setSdt] = useState(cat_data);
+    //  데이터  건수
+    let [tot, setTot] =useState(cat_data.length)
+
+    // 데이터 검색 함수
+    const schList = ()=>{
+        let inp  = document.querySelector("#schin");
+        let keyword = inp.value;
+        // 1.검색어 읽기
+    }
 
 
     return(
@@ -20,11 +31,14 @@ function Search(){
             {/* 모듈코드 */}
             <section className='schbx'>
                 <div className='schopt'>
-
+                    <div className='searching'>
+                        <FontAwesomeIcon icon={faSearch} fade className='schbtn' title='Open Search' onClick={schList}/>
+                        <input id='schin' type='text' placeholder='Filter by Keyword' />
+                    </div>
                 </div>
                 <div className='listbx'>
                     <h2 className='restit'>
-                        BROWSE CHARACTERS
+                        BROWSE CHARACTERS({tot})
                     </h2>
                     <aside className='sortbx'>
 

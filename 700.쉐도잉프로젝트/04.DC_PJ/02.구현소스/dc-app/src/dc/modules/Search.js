6f08463fec2b7b4fd627225cfe920082ca24b3b7
seongwoo83 <jeongseongwoo83@daum.net>
2023-06-26 11:20:08 +0900

    06/26

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/Search.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/Search.js
@@ -59,6 +59,12 @@ function Search(){
     const enterKey = (e)=>{
         if(e.key=== "Enter") schList();
     }
+
+    cat_data.sort((x,y)=>{
+        return  x.cname === y.cname ? 0  : x.cname > y.cname ? 1: -1;
+    })
+
+
     // 리스트 정렬 변경함수
     const sortList = (e)=>{
         // 1. 선택옵션 값
@@ -82,28 +88,155 @@ function Search(){
         setSdt([temp, Number(opt)]);
     }
 
+    // 체크박스 요소
+    let chkele = document.querySelectorAll(".chkhdn");
+
+    const chkSearch = (e) =>{
+        // 1. 체크바스 아이디 : 검색항목의 값(alignment)
+        let cid = e.target.id;
+        // 2. 체크박스 체크여부 : checked(true/false)
+        let checked = e.target.checked;
+        // 3. 체크박스 체크여부에 따른 분기
+        // 체크여부가  true일 떄, 해당 검색어로 검색하기
+
+        // 임시변수 : 기존 입력된 데이터 가져옴
+        let temp = sdt[0];
+        // 결과 집합 변수
+        let newList = [];
+        //  체크박스 체크개수 세기
+        let num = 0;
+        chkele.forEach(v=>{
+            if(v.checked) num++
+        })
+
+        // 
+        
+        if(checked){  // 체크박스 true 일때
+            //  현재 데이터 변수에 담기
+            let nowdt = cat_data.filter(v=>{
+                if(v.alignment === cid) return true;
+            })
+
+            // 체크개수가 1 초과일때 배열 합치기
+            if(num > 1){ // 스프레드 연산자 사용([...])
+                newList = [...temp,  ...nowdt];
+            }else{
+                newList = nowdt;
+            }
+        }else{  // 체크박스가  false 일때 데이터 지우기
+            // splice로 삭제시 일반 for문으로 --처리해야함
+            for(let i = 0; i<temp.length; i++){
+                // 조건은 체크박스 풀린 값
+                if(temp[i].alignment === cid){
+                    // 배열 지우기 메서드 : splice(순번, 갯수)
+                    temp.splice(i, 1);
+                    // 중요! splice로 지우면 배열항목 자체가 삭제되므로 for문 돌 떄 개수가 줄어든다
+                    // 따라서 다음번호를 지울 때 ++을 --처리 필수
+                    i--;
+                }
+            }
+            // 삭제처리된 temp를 결과에 넣기
+            newList = temp;
+        }
+
+        // 4. 검색결과 리스트 업데이트하기
+        // Hook 데이터 변수 + 데이터 건수 업데이트
+        setSdt([newList], 2);
+        setTot(newList.length)
+    }
+
+
 
     return(
         <>
             {/* 모듈코드 */}
-            <section className='schbx'>
-                <div className='schopt'>
-                    <div className='searching'>
-                        <FontAwesomeIcon icon={faSearch} fade className='schbtn' title='Open Search' onClick={schList}/>
-                        <input id='schin' type='text' placeholder='Filter by Keyword' onKeyDown={enterKey}/>
+            <section className="schbx">
+                {/* 1. 옵션선택박스 */}
+                <div className="schopt">
+                    {/* 검색박스 */}
+                    <div className="searching">
+                        {/* 검색버튼 돋보기아이콘 */}
+                        <FontAwesomeIcon
+                            icon={faSearch}
+                            className="schbtn"
+                            title="Open search"
+                            onClick={schList}
+                        />
+                        {/* 입력창 */}
+                        <input
+                            id="schin"
+                            type="text"
+                            placeholder="Filter by Keyword"
+                            onKeyUp={enterKey}
+                        />
+                    </div>
+                    {/* 체크박스구역 */}
+                    <div className="chkbx">
+                        <ul>
+                            <li>
+                                <h2>
+                                    ALIGNMENT
+                                    <span className="spbtn">
+                                        ＋
+                                    </span>
+                                </h2>
+                                {/* 체크박스리스트 */}
+                                <ol>
+                                    <li>
+                                        Heroes
+                                        <input 
+                                        type="checkbox"
+                                        id="hero"
+                                        className="chkhdn"
+                                        onChange={chkSearch} 
+                                        />
+                                        <label
+                                        htmlFor="hero"
+                                        className="chklb"></label>
+                                    </li>
+                                    <li>
+                                        It's Complicated
+                                        <input 
+                                        type="checkbox"
+                                        id="comp"
+                                        className="chkhdn"
+                                        onChange={chkSearch} 
+                                        />
+                                        <label
+                                        htmlFor="comp"
+                                        className="chklb"></label>
+                                    </li>
+                                    <li>
+                                        Villiains
+                                        <input 
+                                        type="checkbox"
+                                        id="villain"
+                                        className="chkhdn"
+                                        onChange={chkSearch} 
+                                        />
+                                        <label
+                                        htmlFor="villain"
+                                        className="chklb"></label>
+                                    </li>
+                                </ol>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-                <div className='listbx'>
-                    <h2 className='restit'>
-                        BROWSE CHARACTERS({tot})
-                    </h2>
-                    <aside className='sortbx'>
-                        <select className='sel' name='sel' id='sel' onChange={sortList}>
+                {/* 2. 결과리스트박스 */}
+                <div className="listbx">
+                    {/* 결과타이틀 */}
+                    <h2 className="restit">BROWSE CHARACTERS({tot})</h2>
+                    {/* 정렬선택박스 */}
+                    <aside className="sortbx">
+                        <select className="sel" name="sel" id="sel" onChange={sortList}>
                             <option value="0">A-Z</option>
                             <option value="1">Z-A</option>
                         </select>
                     </aside>
-                    <CatList dt={sdt[0]}/>
+                    {/* 캐릭터 리스트 컴포넌트 
+                전달속성 dt - 리스트 데이터 */}
+                    <CatList dt={sdt[0]} />
                 </div>
             </section>
             {jqfn()}
